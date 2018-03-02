de8e66c2b351cdaa457517eb559e9cf838ac5b2c
Hareesh Gottipati <hareesh.x.gottipati.-nd@disney.com>
2018-03-01 16:19:11 -0800

    remove sensitive information

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 // How to run the script
 // $MONGO_HOME\bin>mongo.exe -u username -p password server[:port]/Admin [--eval "var param1=value, param2=value"] path_to_script.js
 // eg - 
-// mongo -u user_m4 -p password qn7ddsmng01.starwave.com/m4_TSS  --eval " var sleepBetweenBatches=1000" oneid_qa.js
+// mongo -u user_ -p P@$$word mongo_hostname.domain.com/db  --eval " var sleepBetweenBatches=1000" main.js
 // The following command-line parameters can be set in an eval block:
 
 var sleepBetweenDBs =   sleepBetweenDBs || 400;
 var sleepBetweenBatches =  sleepBetweenBatches || 1000;
 var batchsize = batchsize  || 50
-var m4_dbsPath =  "------------/m4_dbsArray_oneid_qa.js"
+var _dbsPath =  "/path_to_dblist/db_list.js"
 
 //	LIST DBs 
 
-load(m4_dbsPath) 
-var m4dbsToProcess = JSON.parse(m4dbs);
-dbcount = m4dbsToProcess.length
+load(dbsPath) 
+var dbsToProcess = JSON.parse(dbs);
+dbcount = dbsToProcess.length
 print ("Database count " + dbcount)
 print("Databases being processed in this run are as below - ")
-printjson(m4dbsToProcess)
+printjson(dbsToProcess)
 
 
 //   Main script entry
 
-m4dbsToProcess.forEach(function(database)	{
+dbsToProcess.forEach(function(database)	{
 db = db.getSiblingDB(database);      
 if (db != 'local' && db !='admin') 
 	{   
